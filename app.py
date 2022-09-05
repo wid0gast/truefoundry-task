4becf0a2fa0f654eea1aad8c45f8ad6cda165e46
Shaz Furniturewala <mohammadshaz529@gmail.com>
2022-09-05 11:37:25 +0530

    Heroku changes

--- app.py
+++ app.py
@@ -36,4 +36,4 @@ async def predict_sentiment(tweet: Tweet):
         return {'sentiment' : 'positive'}
 
 if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=8000, debug=True)
+    uvicorn.run(app, host='127.0.0.1', debug=True)
